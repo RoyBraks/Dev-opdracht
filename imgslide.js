998beaf59b326f6d66e3bd8325684c49086cacca
RoyBraks <roybraks@gmail.com>
2022-09-08 12:55:47 +0200

    finished

--- imgslide.js
+++ imgslide.js
@@ -1,40 +1,33 @@
 let imgvalue = 1;
-let imgvalueunder = 0;
 
 
 
 swipeimg();
 
-swipeimgunder();
-
 function swipeimg(){
-    setTimeout(swipeimg, 400);
+    
+    let imgslideunder = document.getElementsByClassName("stripslideunder");
+    let r;
+
+    for(r = 0; r < imgslideunder.length; r++){
+       imgslideunder[r].style.display = "none";
+    }
     let imgslide = document.getElementsByClassName("stripslide");
     let i;
+    
     for(i = 0; i < imgslide.length; i++){
        imgslide[i].style.display = "none";
     }
-
+    
     imgvalue++;
+    
     if(imgvalue > imgslide.length){
         imgvalue = 1;
     }
-
-    imgslide[imgvalue-1].style.display = "block";
-}
-
-function swipeimgunder(){
-    setTimeout(swipeimgunder, 400);
-    let imgslideunder = document.getElementsByClassName("stripslideunder");
-    let i;
-    for(i = 0; i < imgslideunder.length; i++){
-       imgslideunder[i].style.display = "none";
-    }
     
-    imgvalueunder++
-    if(imgvalue > imgslideunder.length){
-        imgvalueunder = 0;
-    }
-        
-    imgslideunder[imgvalueunder-1].style.display = "block";
+    imgslideunder[imgvalue-1].style.display = "block";
+    
+    imgslide[imgvalue-1].style.display = "block";
+    
+    setTimeout(swipeimg, 100);
 }
